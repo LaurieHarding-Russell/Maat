48353b20b4efc5882b278dd8486f490b3bb57541
Ben Leech <b.leech@hotmail.ca>
2018-05-12 13:45:00 -0600

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {SearchComponent} from "./search/search.component";
+import {RouterModule, Routes} from "@angular/router";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 const routes: Routes = [
   {
     path: '',
-    component: 
+    component: SearchComponent
   },
 
 
-]
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
