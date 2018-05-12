9f989fa7ad5f9cd44c262e4035ff9cc9615a2814
Unknown <b.leech@hotmail.ca>
2018-05-12 11:17:19 -0600

    added app routing module

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
 
-@Component({selector: 'app-component', template: '<maat-app></maat-app>'})
+@Component({selector: 'app-component',
+    template: `        
+        <router-outlet></router-outlet>
+    `
+})
 export class AppComponent {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {MaatModule} from './maat/maat.module';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, MaatModule],
+  imports: [BrowserModule, MaatModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
