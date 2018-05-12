64c34cfb2064837cc7a99ff6509b39f6f22549a2
Ben Leech <b.leech@hotmail.ca>
2018-05-12 15:53:45 -0600

    query params on search

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'company',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
-    
+export class ResultsComponent implements OnInit {
+
+  searchControl: FormControl = new FormControl(null);
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {}
+
+  ngOnInit() {
+
+  }
+
+  onSearch() {
+    const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
+    queryParams['search'] = this.searchControl.value;
+
+    this.router.navigate(['./results'], {queryParams: queryParams})
+  }
+
 }
