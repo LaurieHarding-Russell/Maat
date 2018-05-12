89d83ea0daa2f94bc5e8a423ef7ccc13e72c7fa9
Ben Leech <b.leech@hotmail.ca>
2018-05-12 13:48:01 -0600

    adding to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {SearchComponent} from "./search/search.component";
+import {ProductComponent} from "./product/product.component";
+import {ResultsComponent} from "./results/results.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      SearchComponent,
+      ProductComponent,
+      ResultsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
