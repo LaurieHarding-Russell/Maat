c7ca3dc188e168767c357a533f9f2f3bac91509c
LaurieHarding-Russell <lauriehardingrussell@gmail.com>
2018-05-11 19:18:00 -0600

    Starting the project...

---
+++ src/maat/hello-world.component.ts
@@ -0,0 +1,12 @@
+import {Component, NgModule} from '@angular/core';
+
+@Component({
+  selector: 'hello-world-app',
+  template: `
+    <div>Hello {{ name }}</div>
+    <input type="text" [value]="name" (input)="name = $event.target.value"/>
+  `,
+  styleUrls: ['./hello-world-styles.css']
+})
+export class HelloWorldComponent {
+}
