316bcc0e25bf5161b485311751c16fe0fe1c08f1
Ben Leech <b.leech@hotmail.ca>
2018-05-12 17:26:27 -0600

    added java spring boot

---
+++ java/maat/src/main/java/com/moose/maat/MaatApplication.java
@@ -0,0 +1,12 @@
+package com.moose.maat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaadApplication.class, args);
+	}
+}
