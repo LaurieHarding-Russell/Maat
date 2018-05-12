e1722b1443e9c04d6bf0fe74628bfd08f411cfe4
LaurieHarding-Russell <lauriehardingrussell@gmail.com>
2018-05-11 21:57:54 -0600

    Company

---
+++ src/maat/company/company.component.ts
@@ -0,0 +1,11 @@
+import {Component, NgModule} from '@angular/core';
+
+@Component({
+  selector: 'maat-app',
+  template: `
+    <div> Company </div>
+  `,
+  styleUrls: ['./maat-styles.css']
+})
+export class CompanyComponent {
+}
