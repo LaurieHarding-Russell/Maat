7b2c2a280db828e7b92989dfa830f2250eb6291e
Ben Leech <b.leech@hotmail.ca>
2018-05-12 19:29:03 -0600

    created search result dto

---
+++ src/main/java/com/moose/maat/model/SearchResultDto.java
@@ -0,0 +1,11 @@
+package com.moose.maat.model;
+
+import java.math.BigDecimal;
+
+public class SearchResultDto {
+	String name;
+	BigDecimal rating;
+	String description;
+	String imagePath;
+	String routeId;
+}
