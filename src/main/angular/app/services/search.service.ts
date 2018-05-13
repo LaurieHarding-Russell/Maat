998a105d84a8d5ff40b2ea1bf1b28b7a149fb901
Ben Leech <b.leech@hotmail.ca>
2018-05-13 10:56:45 -0600

    fixing build errors

--- src/main/angular/app/services/search.service.ts
+++ src/main/angular/app/services/search.service.ts
@@ -8,13 +8,13 @@ const SERVICE_URL = '../api/search';
 @Injectable()
 export class SearchService {
 
-    private searchResults$: Subject<SearchResult> = new Subject<SearchResult>();
+    private searchResults$: Subject<Array<SearchResult>> = new Subject<Array<SearchResult>>();
     searchResults = this.searchResults$.asObservable();
 
     constructor(private http: HttpClient){}
 
     getCompanies(companyName: string) {
-        return this.http.get(`//localhost:8080/api/search/${companyName}`).subscribe(results => {
+        return this.http.get(`//localhost:8080/api/search/${companyName}`).subscribe((results: Array<SearchResult>) => {
             this.searchResults$.next(results);
         });
     }
