42b81726b5b9f2cc817e46f49266600d79c9ffd0
Ben Leech <b.leech@hotmail.ca>
2018-05-12 20:30:00 -0600

    more work on clearbit api

--- src/main/java/com/moose/maat/controller/SearchController.java
+++ src/main/java/com/moose/maat/controller/SearchController.java
@@ -1,12 +1,23 @@
 package com.moose.maat.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.moose.maat.core.SearchService;
+import com.moose.maat.model.SearchResultDto;
 
 @RestController
 @RequestMapping("/api/search")
 public class SearchController {
-
 	
+	@Autowired
+	private SearchService searchService;
+
+	public List<SearchResultDto> getCompanies(String companyName) {
+		return searchService.getCompanies(companyName);
+	}
 	
 }
